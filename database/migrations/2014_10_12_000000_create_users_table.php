f3b93c282915831439db51b2f43bd4e349d01815
Tudor Ionut Elian <ionuteliantudor@gmail.com>
2022-11-29 22:47:08 +0200

    Remove extra migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('u_name');
+            $table->string('u_email')->unique();
+            $table->timestamp('u_email_verified_at')->nullable();
+            $table->string('u_token');
             $table->timestamps();
         });
     }
