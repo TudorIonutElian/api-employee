61ec541f9c6b5a46685ec81af6ca31cfd7e52975
Tudor Ionut Elian <ionuteliantudor@gmail.com>
2022-12-03 17:16:43 +0200

    Create Employees table

--- database/migrations/2022_12_03_151203_create_employees_table.php
+++ database/migrations/2022_12_03_151203_create_employees_table.php
@@ -14,8 +14,23 @@ return new class extends Migration
     public function up()
     {
         Schema::create('employees', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id('e_id');
+            $table->string('e_first_name', 150);
+            $table->string('e_last_name', 150);
+            $table->date('e_dob');
+            $table->string('e_identity', 13);
+            $table->unsignedBigInteger('e_city_id')->nullable();
+            $table->unsignedBigInteger('e_region_id')->nullable();
+            $table->unsignedBigInteger('e_company_id')->nullable();
+            $table->unsignedBigInteger('e_function_id')->nullable();
+            $table->unsignedBigInteger('e_salary_id')->nullable();
+            $table->unsignedBigInteger('e_created_by_id');
+
+            $table->foreign('e_created_by_id')
+                    ->references('u_id')
+                    ->on('users')
+                    ->onUpdate('cascade')
+                    ->onDelete('cascade');
         });
     }
 
